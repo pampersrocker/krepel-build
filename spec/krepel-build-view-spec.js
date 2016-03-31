ab681df1b402c6da083d2def72a9335cab8a481a
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-03-31 08:47:29 +0200

    Initial commit

---
+++ spec/krepel-build-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KrepelBuildView from '../lib/krepel-build-view';
+
+describe('KrepelBuildView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
